954acf77a740f9ecb18a2ab438269d40ca8d7107
zapjellyp <ongdongyingr651@163.com>
2021-07-07 07:52:11 +0800

    布局

--- koala-dmt/koala-dmt-web/src/main/webapp/js/main.js
+++ koala-dmt/koala-dmt-web/src/main/webapp/js/main.js
@@ -6,8 +6,8 @@ $(function() {
 		$('#gMain').height(0.89*height);
 		$('#gFooter').height(0.05*height);
 		$('#projectExplorer .panel-body').height(0.36*height);
-		$('#propertiesExplorer .panel-body').height(0.21*height);
-		$('#gwtTabLayoutPanel .panel-body').height(0.72*height)
+		$('#propertiesExplorer .panel-body').height(0.23*height);
+		$('#gwtTabLayoutPanel .panel-body').height(0.74*height)
 	});
 	$(window).trigger('resize');
 });
