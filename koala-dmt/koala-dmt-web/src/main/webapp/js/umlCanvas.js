5d30da60522bc4b3f726c9fa53d1a74d0e440e7d
zapjellyp <ongdongyingr651@163.com>
2021-08-11 12:35:41 +0800

    Ìí¼ÓÓҼü±༭²˵¥£»½«´úÂëÖع¹³Éjquery²å¼þ

--- koala-dmt/koala-dmt-web/src/main/webapp/js/umlCanvas.js
+++ koala-dmt/koala-dmt-web/src/main/webapp/js/umlCanvas.js
@@ -11,13 +11,14 @@ function umlCanvas(){
 	var umlCanvas 		= $("#uml_canvas");			//所有图形元素的容器
 	var canvas_offset 	= umlCanvas.offset();		//总容器的位置
 	var focusItem 		= null;						//当前被选中的元素
-	var toolBar			= $("#tool_bar");			//切换工具的工具栏
+	var toolBar			= $("#tools");			//切换工具的工具栏
 	var curTool			= {type:"cursor",name:null};//当前的工具（currentTool）
 	var focusItem		= null;						//当前被选中的节点或线条
 	
 	//全局缓存
 	var lines 		= []; 	//页面上的所有线条对象以及所有线条的控制数据(json格式)
 	var nodes 		= []; 	//页面上的所有节点对象以及所有节点的控制数据(json格式)
+	
 	
 	/*拖动鼠标画连线*/
 	var drawing 	= false; 				//标志当前状态是否划线状态
@@ -85,17 +86,24 @@ function umlCanvas(){
 			};
 			commonTool.maintainDataList("property_type_tip",1,"class",id);
 		}
+		
+		/*当画布被点击一次时，如果当前不是线条工具，将工具切换回鼠标工具*/
+		if(curTool.type == "node"){
+			swichTool("cursor");
+		}
 	});
 	
 	/*点击节点，添加属性或行为*/
 	umlCanvas.delegate(".node","click",function(e){
-		if(curTool.name == "attribute"){	//添加属性
+		if(curTool.name == "property"){	//添加属性
 			$(this).find(".properties").append($("#node-template .property").clone());
 		} else if(curTool.name == "action"){ //添加行为
 			$(this).find(".actions").append($("#node-template .action").clone());
 		}
 		var relatedLines = commonTool.findRelatedLines($(this).attr("id"),lines);
 		svgGraph.resetLines($(this),nodes,lines,relatedLines.outLines,relatedLines.inLines);
+		
+		swichTool("cursor");
 	});
 	
 	/*拖拽节点*/
@@ -133,36 +141,15 @@ function umlCanvas(){
 		$(this).unbind("mousemove",drag);
 	});
 	
-	
 	/*点击工具切换*/
 	$("#tools").delegate(".node,.line,.component","click",function(e){
-		if($(this).is(".node")){
-			curTool.type = "node";
-		} else {
-			curTool.type = "line";
-		}
-		curTool.name = $(this).attr("class").split(" ")[1];
-		
-		$("#tools").find(".current-tool").removeClass("current-tool");
-		$(this).addClass("current-tool");
-	});
-	
-	/*右键把工具切换为“鼠标工具”*/
-	umlCanvas.contextmenu(function(e){
-		if(e.button == 2){
-			e.preventDefault();
-			curTool.type = "cursor";
-			curTool.name = null;
-			
-			$("#tools").find(".current-tool").removeClass("current-tool");
-			$("#tools .cursor").addClass("current-tool");
-		}
+		swichTool($(this).attr("class").split(" ")[1]);
 	});
 	
 	/** 
-	 * 编辑节点名字并显示添加属性和行为操作按钮 
+	 * 编辑节点名字 
 	 */
-	$("#uml_canvas").delegate(".name","dblclick",function(e){
+	umlCanvas.delegate(".name","dblclick",function(e){
 		/*编辑节点名字*/
 		$(this).miniedit({
 			type:'input',
@@ -179,8 +166,8 @@ function umlCanvas(){
 		});
 	});
 	
-	/* 编辑属性或行为 */
-	$("#uml_canvas").delegate(".properties,.actions","dblclick",function(e){
+	/* 双击方式编辑属性或行为 */
+	umlCanvas.delegate(".properties,.actions,.name","dblclick",function(e){
 		e.preventDefault();
 		var thiz = $(this),t=$(e.target);
 		
@@ -233,183 +220,61 @@ function umlCanvas(){
 		//节点尺寸改变，需要重画连线
 		
 	});
-};
-
-umlCanvas.prototype = {
-	/*线条画笔工具*/
-	svgGraph : {
-		/*画一条线*/
-		drawLine : function(start ,end , name){
-			var line = $("#line-template ."+name).clone();
-			var path = line.children();
-			
-			/*线条的路径*/
-			path.attr("d", 
-				"M" + start[0] 	+ "," + start[1] 	+ " " + 
-				"L" + end[0] 	+ "," + end[1] 		+ " ");
-				
-			return line;
-		} ,
-		
-		/*移动一条直线*/
-		moveLine : function(line ,start ,end){
-			line.children("path").attr("d", 
-				"M" + start[0] 	+ "," + start[1] 	+ " "+
-				"L" + end[0] 	+ "," + end[1] 		+ " ");
-		},
-		
-		//重构所有连向某个结点的线的显示，传参结构为nodes数组的一个单元结构
-		resetLines : function(node, nodes, lines, outLines, inLines) {
-			var line,startNode,endNode;
-			var endPoints;
-			
-			/*重画指出的线*/
-			for(var i in outLines){
-				endpoints = this.getStartEnd(node,nodes[outLines[i].to].nodeObj);
-				this.moveLine(lines[i].lineObj, endpoints.start, endpoints.end);
-			}
-			
-			/*重画指入的线*/
-			for(var i in inLines){
-				endpoints = this.getStartEnd(nodes[inLines[i].from].nodeObj,node);
-				this.moveLine(lines[i].lineObj, endpoints.start, endpoints.end);
-			}
-		},
-		
-		/*在画直线时，根据两个节点计算连线起止点*/
-		getStartEnd : function(n1, n2) {
-			var start = [],	end = [];
-			//左右判断：
-			var x11 = n1.position().left , x12 = n1.position().left + n1.outerWidth(), 
-				x21 = n2.position().left , x22 = n2.position().left + n2.outerWidth();
-				
-			//结点2在结点1左边
-			if (x11 >= x22) {
-				start[0] = x11;
-				end[0] = x22;
-			} else if (x12 <= x21) {
-				start[0] = x12;
-				end[0] = x21;
-			} else if (x11 <= x21 && x12 >= x21 && x12 <= x22) { //结点2在结点1水平部分重合
-				start[0] = (x12 + x21) / 2;
-				end[0] = start[0];
-			} else if (x11 >= x21 && x12 <= x22) {
-				start[0] = (x11 + x12) / 2;
-				end[0] = start[0];
-			} else if (x21 >= x11 && x22 <= x12) {
-				start[0] = (x21 + x22) / 2;
-				end[0] = start[0];
-			} else if (x11 <= x22 && x12 >= x22) {
-				start[0] = (x11 + x22) / 2;
-				end[0] = start[0];
-			}
 	
-			//上下判断：
-			var y11 = n1.position().top, y12 = n1.position().top + n1.outerHeight(), 
-				y21 = n2.position().top, y22 = n2.position().top + n2.outerHeight();
-				
-			//结点2在结点1上边
-			if (y11 >= y22) {
-				start[1] = y11;
-				end[1] = y22;
-			} else if (y12 <= y21) { //结点2在结点1下边
-				start[1] = y12;
-				end[1] = y21;
-			} else if (y11 <= y21 && y12 >= y21 && y12 <= y22) { //结点2在结点1垂直部分重合
-				start[1] = (y12 + y21) / 2;
-				end[1] = start[1];
-			} else if (y11 >= y21 && y12 <= y22) {
-				start[1] = (y11 + y12) / 2;
-				end[1] = start[1];
-			} else if (y21 >= y11 && y22 <= y12) {
-				start[1] = (y21 + y22) / 2;
-				end[1] = start[1];
-			} else if (y11 <= y22 && y12 >= y22) {
-				start[1] = (y11 + y22) / 2;
-				end[1] = start[1];
-			}
-			
-			return {
-				"start" : start,
-				"end" 	: end
-			};
+	/*右键添加属性或行为或枚举项等等*/
+	umlCanvas.delegate(".node","contextmenu",function(e){
+		e.preventDefault();
+		var thiz = $(this);
+		var position = commonTool.mousePosition(e);
+		
+		if(thiz.is(".entity")){ 			//右键实体，添加属性和行为
+			showContextmenu(e,"add_members");
+		} else if(thiz.is(".interface")){ 	//右键接口，添加行为和常量
+			alert(2);
+		} else if(thiz.is(".enum")){ 		//右键枚举，添加枚举项
+			alert(3);
 		}
-	},
-
-	/*画html节点的画笔*/
-	domGraph : {
-		/*添加节点*/
-		addNode : function(dom,position,canvas,id){
-			canvas.append(dom);
-			dom.css({
-				left:position.left,
-				top:position.top
-			}).attr("id",id);
-			return dom;
-		},
+	});
+	
+	$("#edit_contextmenus").delegate(".contextmenu","click",function(){
+		$(this).hide();
+	});
+	
+	/*切换工具的方法*/
+	function swichTool(name){
+		var tool = toolBar.find("."+name);
+		var curtool  = tool.attr("class").split(" ");
 		
-		/*选中节点*/
-		focusItem : function(){
-			
-		},
+		curTool.type = curtool[0];
+		curTool.name = curtool[1];
 		
-		/*删除节点时，需要级联删除连线和与连线有关系的属性*/
-		deleteNode : function(){
-			
-		},
+		toolBar.find(".current-tool").removeClass("current-tool");
+		tool.addClass("current-tool");
+	}
+	
+	/*显示右键菜单*/
+	function showContextmenu(e,menuName){
+		var position = commonTool.mousePosition(e);
 		
-		/*更新节点时，需要级联更新*/
-		updateNode : function(){
-			
-		}
-	},
-	/*
-	 * 部分关系到节点和连线的操作，以及所有跟连线和节点无关的操作放在commonTool里
-	 */
-	commonTool : {
-		/*用随机数作为元素的id*/
-		guid : function() {
-			function s4() {
-				return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-			}
-		  	return s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
-		},
+		$("#"+menuName).css({
+			display	: "block",
+			top		: position.top,
+			left	: position.left
+		});
+	}
+	
+	/*添加属性*/
+	function addProperty(target){
 		
-		/*找出和某个节点连接的所有线条（分连出和连入两种）*/
-		findRelatedLines : function(nodeId,lines){
-			var outLines = [], inLines = [];
-			for(var i in lines){
-				if(lines[i].to == nodeId){
-					inLines[i] = lines[i];
-				} else if(lines[i].from == nodeId){
-					outLines[i] = lines[i];
-				}
-			}
-			
-			return {
-				outLines : outLines,
-				inLines : inLines
-			};
-		},
+	}
+	
+	/*添加行为*/
+	function addAction(target){
 		
-		/*维护输入提示信息，包括添加、删除等操作.option参数为2时表示更新，为1时表示添加，为0时表示删除*/
-		maintainDataList : function(listId,option,data,classSelector){
-			var dataList = $("#"+listId);
-			if(option == 1 && dataList.find("."+classSelector).length == 0){
-				dataList.append($("<option/>").val(data).addClass(classSelector));
-			} else if(option == 2){
-				dataList.find("."+classSelector).val(data);
-			} else if(option ==  0){
-				$("#"+listId).remove("."+classSelector);
-			}
-		},
+	}
+	
+	/*添加枚举项*/
+	function addEnumItem(target){
 		
-		/**
-		 * 当节点的dom对象信息更新时，对应的节点缓存信息同步更新。导致调用该方法的事件 包括
-		 * 节点名、属性名、属性类型、行为名、行为参数名、行为参数类型、行为返回值等信息更改
-		 */
-		maintainNodesData : function(){
-			
-		}
 	}
 };
