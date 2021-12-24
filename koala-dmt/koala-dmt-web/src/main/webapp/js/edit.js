b5e261c8f5d04085a717be1930b1da22c7d0e7be
zapjellyp <ongdongyingr651@163.com>
2021-12-24 12:50:02 +0800

    初步配色，重构页面

--- koala-dmt/koala-dmt-web/src/main/webapp/js/edit.js
+++ koala-dmt/koala-dmt-web/src/main/webapp/js/edit.js
@@ -149,13 +149,15 @@ function addEnumItem(target){
 
 /*↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓所有更新操作↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓*/
 /*更新名字，需要级联更改自动生成的属性的类型名*/
-function updateNodeName(target, newName, canvas){
-	/*该节点的所有连线*/
-	var inAout 	= commonTool.findRelatedLines(target.attr("id"),canvas.LINES);
-	var data	= target.data("data");
+function updateNodeName(node, input, canvas){
+	checkNodeName(node, input, canvas);
 	
-	var ins 	= inAout.inLines;
-	var outs 	= inAout.outLines;
+	/*该节点的所有连线*/
+	var inAout 	= commonTool.findRelatedLines(node.attr("id"),canvas.LINES),
+		newName = input.val(),
+		data	= node.data("data"),
+		ins 	= inAout.inLines,
+		outs 	= inAout.outLines;
 	
 	var node,model; //node and domainmodel
 	$.each(ins,function(i, line){
@@ -212,7 +214,7 @@ function updateNodeName(target, newName, canvas){
 	}
 	
 	data.name 	= newName;
-	target.find(".name").html(newName);
+	node.find(".name").html(newName);
 }
 
 /*更新实体类型*/
@@ -295,20 +297,21 @@ function updateEnumName(enumDom, val){
 /*↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑所有更新操作↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑*/
 
 /*↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓所有检查操作↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓*/
-function checkNodeName(name, node, canvas){
+function checkNodeName(node, name, canvas){
 	var models 	= canvas.MODELS,
-		val 	= name.val();
+		val 	= name.val(),
+		model	= node.data("data");
 		
 	for(var i in models){
-		if(val == models[i].name){
+		if(val == models[i].name && models[i].shapeId != model.shapeId){
 			name.addClass("duplication_name"); /*重名*/
 			return;
 		}
 	}
-	console.log(2232323);
 	name.removeClass("duplication_name");/*不重名*/
 }
 /*↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑所有检查操作↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑*/
+
 
 /*↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓所有删除操作↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓*/
 /*删除节点*/
